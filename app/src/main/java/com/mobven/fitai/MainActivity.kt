29d205f1974a8929aab3a83e66320177bc512bb7
Furkan <kvkfurkan@hotmail.com>
2024-05-17 13:11:16 +0300

    auth fragment added.

--- app/src/main/java/com/mobven/fitai/MainActivity.kt
+++ app/src/main/java/com/mobven/fitai/MainActivity.kt
@@ -1,11 +1,10 @@
-package com.mobven.fitai.activities
+package com.mobven.fitai
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.mobven.fitai.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/mobven/fitai/login/AuthFragment.kt
+++ app/src/main/java/com/mobven/fitai/login/AuthFragment.kt
@@ -1,4 +1,4 @@
-package com.mobven.fitai.activities.fragments
+package com.mobven.fitai.login
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
