04da659c33ea936a5f51a7b51419b1c8b5d63ea9
Mert Ozan Kahraman <mozankahraman69@gmail.com>
2024-05-17 12:33:38 +0300

    Navigation implemented

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,10 +45,13 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    testImplementation(libs.bundles.testImplementation)
+    androidTestImplementation(libs.bundles.androidTestImplementation)
 
+    // Navigation
+    implementation(libs.bundles.navigation)
+
+    // Viewpager Indicator - 3rd Party
     implementation(libs.thirdparty.viewpagerdotsindicator)
 
 }
