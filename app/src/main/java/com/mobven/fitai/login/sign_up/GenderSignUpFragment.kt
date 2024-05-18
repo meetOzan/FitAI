4c68451507c0675c38e01a00323e5e29876c2c84
Mert Ozan Kahraman <mozankahraman69@gmail.com>
2024-05-19 02:11:24 +0300

    GenderFragment added

---
+++ app/src/main/java/com/mobven/fitai/login/sign_up/GenderSignUpFragment.kt
@@ -0,0 +1,47 @@
+package com.mobven.fitai.login.sign_up
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.mobven.fitai.R
+import com.mobven.fitai.databinding.FragmentGenderSignUpBinding
+import com.mobven.fitai.login.sign_up.adapter.SignUpListAdapter
+
+class GenderSignUpFragment : Fragment() {
+
+    private lateinit var binding: FragmentGenderSignUpBinding
+    private val adapter by lazy { SignUpListAdapter(requireContext()) }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentGenderSignUpBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val genderList = listOf(
+            SignUpSelectorItem(
+                title = getString(R.string.female_gender),
+                isSelected = false
+            ),
+            SignUpSelectorItem(
+                title = getString(R.string.male_gender),
+                isSelected = false
+            ),
+            SignUpSelectorItem(
+                title = getString(R.string.other_gender),
+                isSelected = false
+            )
+        )
+
+        adapter.submitList(genderList)
+        binding.rvGender.adapter = adapter
+
+    }
+}
---
+++ app/src/main/java/com/mobven/fitai/login/sign_up/SignUpSelectorItem.kt
@@ -0,0 +1,6 @@
+package com.mobven.fitai.login.sign_up
+
+data class SignUpSelectorItem(
+    val title: String,
+    var isSelected: Boolean
+)
