4e8c9713cc21abdfc82702d5960c4f8103960d3c
Mert Ozan Kahraman <mozankahraman69@gmail.com>
2024-05-19 02:12:50 +0300

    Personal info fragments added

---
+++ app/src/main/java/com/mobven/fitai/login/sign_up/HeightSignUpFragment.kt
@@ -0,0 +1,19 @@
+package com.mobven.fitai.login.sign_up
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.mobven.fitai.R
+
+class HeightSignUpFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_height_sign_up, container, false)
+    }
+
+}
