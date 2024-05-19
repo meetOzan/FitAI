eceb355cbda0d953fbafbc108712dcd5d37d22f5
Mert Ozan Kahraman <mozankahraman69@gmail.com>
2024-05-19 12:45:50 +0300

    Habits fragments added

--- app/src/main/java/com/mobven/fitai/MainActivity.kt
+++ app/src/main/java/com/mobven/fitai/MainActivity.kt
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.mobven.fitai.databinding.ActivityMainBinding
-import com.mobven.fitai.login.sign_up.SportBodyPartFragment
+import com.mobven.fitai.login.sign_up.AverageSleepFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -26,7 +26,7 @@ class MainActivity : AppCompatActivity() {
         val fragmentManager = supportFragmentManager
 
         val transaction = fragmentManager.beginTransaction()
-        val fragment = SportBodyPartFragment()
+        val fragment = AverageSleepFragment()
         transaction.replace(R.id.fragment_container, fragment)
         transaction.addToBackStack(null)
         transaction.commit()
---
+++ app/src/main/java/com/mobven/fitai/login/sign_up/DailyMovementFragment.kt
@@ -0,0 +1,63 @@
+package com.mobven.fitai.login.sign_up
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.mobven.fitai.R
+import com.mobven.fitai.databinding.FragmentDailyMovementBinding
+import com.mobven.fitai.login.sign_up.adapter.SignUpListAdapter
+import com.mobven.fitai.login.sign_up.model.SignUpSelectorItem
+import com.mobven.fitai.util.SignUpSelectorType
+
+
+class DailyMovementFragment : Fragment() {
+
+    private lateinit var binding: FragmentDailyMovementBinding
+    private var adapter = SignUpListAdapter()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentDailyMovementBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val dailyMovementList = listOf(
+            SignUpSelectorItem(
+                title = getString(R.string.almost_never),
+                isSelected = false,
+                type = SignUpSelectorType.RADIO,
+            ),
+            SignUpSelectorItem(
+                title = getString(R.string.rarely),
+                isSelected = false,
+                type = SignUpSelectorType.RADIO,
+            ),
+            SignUpSelectorItem(
+                title = getString(R.string.sometimes),
+                isSelected = false,
+                type = SignUpSelectorType.RADIO,
+            ),
+            SignUpSelectorItem(
+                title = getString(R.string.often),
+                isSelected = false,
+                type = SignUpSelectorType.RADIO,
+            ),
+            SignUpSelectorItem(
+                title = getString(R.string.always),
+                isSelected = false,
+                type = SignUpSelectorType.RADIO,
+            )
+        ).reversed()
+
+        adapter.submitList(dailyMovementList)
+        binding.rvDailyMovement.adapter = adapter
+
+    }
+}
