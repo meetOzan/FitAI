d64b86276626d0c619c9b294a842fa064c498926
Mert Ozan Kahraman <mozankahraman69@gmail.com>
2024-05-19 03:08:58 +0300

    Birthday Fragment added

---
+++ app/src/main/java/com/mobven/fitai/login/sign_up/BirthdaySignUpFragment.kt
@@ -0,0 +1,50 @@
+package com.mobven.fitai.login.sign_up
+
+import android.app.DatePickerDialog
+import android.icu.util.Calendar
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.mobven.fitai.databinding.FragmentBirthdaySignUpBinding
+
+class BirthdaySignUpFragment : Fragment() {
+
+    private lateinit var binding: FragmentBirthdaySignUpBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentBirthdaySignUpBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.etBirthday.setOnClickListener {
+            val calendar = Calendar.getInstance()
+            val year = calendar[Calendar.YEAR]
+            val month = calendar[Calendar.MONTH]
+            val dayOfMonth = calendar[Calendar.DAY_OF_MONTH]
+
+            val datePickerDialog = DatePickerDialog(
+                requireContext(),
+                { _, selectedYear, selectedMonth, dayOfMonth ->
+                    val selectedDate = "$dayOfMonth/${selectedMonth + 1}/$selectedYear"
+                    binding.etBirthday.setText(selectedDate)
+                },
+                year,
+                month,
+                dayOfMonth
+            )
+
+            datePickerDialog.datePicker.maxDate = System.currentTimeMillis()
+            datePickerDialog.show()
+        }
+
+    }
+
+}
