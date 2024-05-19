121e3161e41aa1c63a57ccb05a72ffe0b13232c8
Mert Ozan Kahraman <mozankahraman69@gmail.com>
2024-05-19 15:08:14 +0300

    PlanCreatingFragment added

---
+++ app/src/main/java/com/mobven/fitai/login/sign_up/PlanCreatingFragment.kt
@@ -0,0 +1,22 @@
+package com.mobven.fitai.login.sign_up
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.mobven.fitai.databinding.FragmentPlanCreatingBinding
+
+class PlanCreatingFragment : Fragment() {
+
+    private lateinit var binding: FragmentPlanCreatingBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentPlanCreatingBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+}
