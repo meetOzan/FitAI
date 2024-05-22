aaab2c97aace4f4bb31ed02ee23230a2c33a6300
Mert Ozan Kahraman <mozankahraman69@gmail.com>
2024-05-22 19:38:27 +0300

    HomeScreen fragment base UI drew

---
+++ app/src/main/java/com/mobven/fitai/home/HomeFragment.kt
@@ -0,0 +1,6 @@
+package com.mobven.fitai.home
+
+import com.mobven.fitai.base.BaseFragment
+import com.mobven.fitai.databinding.FragmentHomeBinding
+
+class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate)
