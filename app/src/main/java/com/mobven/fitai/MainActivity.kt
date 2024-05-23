d8ada6a6d82c5bf268e648dd34a6141d5c067668
Furkan <kvkfurkan@hotmail.com>
2024-05-23 18:11:14 +0300

    otp fragments added.

--- app/src/main/java/com/mobven/fitai/MainActivity.kt
+++ app/src/main/java/com/mobven/fitai/MainActivity.kt
@@ -6,6 +6,8 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.mobven.fitai.databinding.ActivityMainBinding
+import com.mobven.fitai.onboarding.OnboardingFragment
+
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
