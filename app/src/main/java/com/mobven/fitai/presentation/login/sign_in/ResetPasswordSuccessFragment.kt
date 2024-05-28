fb7f3cdb7172d8524d42fe7bd762750fffe5cee7
Mert Ozan Kahraman <mozankahraman69@gmail.com>
2024-05-28 15:42:00 +0300

    BaseFragment inherited by Sign In screens

--- app/src/main/java/com/mobven/fitai/presentation/login/sign_in/ResetPasswordSuccessFragment.kt
+++ app/src/main/java/com/mobven/fitai/presentation/login/sign_in/ResetPasswordSuccessFragment.kt
@@ -3,4 +3,4 @@ package com.mobven.fitai.presentation.login.sign_in
 import com.mobven.fitai.databinding.FragmentResetPasswordBinding
 import com.mobven.fitai.presentation.base.BaseFragment
 
-class ResetPasswordFragment : BaseFragment<FragmentResetPasswordBinding>(FragmentResetPasswordBinding::inflate)
+class ResetPasswordSuccessFragment : BaseFragment<FragmentResetPasswordBinding>(FragmentResetPasswordBinding::inflate)
