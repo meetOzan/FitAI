fb4dcfe90beb31c8cee43d96147e83e792d8b2fb
Mert Ozan Kahraman <mozankahraman69@gmail.com>
2024-05-30 14:51:33 +0300

    Home screen base ui re-designed

--- app/src/main/java/com/mobven/fitai/presentation/home/HomeFragment.kt
+++ app/src/main/java/com/mobven/fitai/presentation/home/HomeFragment.kt
@@ -1,15 +1,13 @@
 package com.mobven.fitai.presentation.home
 
-import android.view.View
 import android.os.Build
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.viewModels
 import androidx.navigation.NavDirections
 import androidx.navigation.fragment.findNavController
 import com.mobven.fitai.R
-import com.mobven.fitai.presentation.base.BaseFragment
-import com.mobven.fitai.databinding.CardPlanItemBinding
 import com.mobven.fitai.databinding.FragmentHomeBinding
+import com.mobven.fitai.presentation.base.BaseFragment
 import com.mobven.fitai.presentation.home.calendar.CalendarItem
 import com.mobven.fitai.presentation.home.calendar.HomeCalendarAdapter
 import com.mobven.fitai.presentation.home.viewmodel.HomeViewModel
@@ -21,13 +19,11 @@ typealias HDirections = HomeFragmentDirections
 
 @AndroidEntryPoint
 class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate) {
-
     private val adapter = HomeCalendarAdapter()
-    private val homeViewModel : HomeViewModel by viewModels()
+    private val homeViewModel: HomeViewModel by viewModels()
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun observeUi() {
-
         homeViewModel.homeUiState.observe(viewLifecycleOwner) { homeState ->
             when {
                 homeState.isError -> {
@@ -41,7 +37,6 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::infl
                 else -> {
                     handleSuccess()
                 }
-
             }
         }
     }
@@ -50,38 +45,9 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::infl
         findNavController().navigate(action)
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     private fun handleSuccess() {
         with(binding) {
-
-            setupCards(
-                includeFoodPlanItem,
-                "Kahvaltı",
-                "10 dakikada hazır",
-                "800 kalori",
-                image = R.drawable.food_plan_item
-            )
-            includeFoodPlanItem.thirdTv.visibility = View.GONE
-            includeFoodPlanItem.thirdIcon.visibility = View.GONE
-
-            setupCards(
-                includeTrainingItem,
-                "Evde Fitness",
-                "50 dakika",
-                "2 Set, 3 Tekrar",
-                "400 kalori",
-                R.drawable.training_plan_item
-            )
-
-            ivFoodPlanAdd.setOnClickListener {
-                val foodAction = HDirections.actionHomeFragmentToNutritionFragment()
-                navigate(foodAction)
-            }
-
-            ivTrainingPlanAdd.setOnClickListener {
-                val trainingAction = HDirections.actionHomeFragmentToTrainingFragment()
-                navigate(trainingAction)
-            }
-
             val dateList = mutableListOf<CalendarItem>()
             val dayFormatter = DateTimeFormatter.ofPattern(getString(R.string.eeee))
             val dateFormatter = DateTimeFormatter.ofPattern(getString(R.string.dd))
@@ -95,16 +61,14 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::infl
                     CalendarItem(
                         date = dateStr,
                         dayName = dayName,
-                        isSelected = isSelected
-                    )
+                        isSelected = isSelected,
+                    ),
                 )
             }
 
             adapter.submitList(dateList)
-            binding.rvHomeCalendar.adapter = adapter
-
+            rvHomeCalendar.adapter = adapter
         }
-
     }
 
     private fun handleError(error: String) {
@@ -114,21 +78,4 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::infl
     private fun handleLoading() {
         println(getString(R.string.loading))
     }
-
 }
-
-private fun setupCards(
-    item: CardPlanItemBinding,
-    header: String,
-    firstTv: String,
-    secondTv: String,
-    thirdTv: String = "",
-    image: Int
-) {
-    item.tvPlanName.text = header
-    item.firstTv.text = firstTv
-    item.secondTv.text = secondTv
-    item.thirdTv.text = thirdTv
-    item.ivCircle.setImageResource(image)
-}
-
