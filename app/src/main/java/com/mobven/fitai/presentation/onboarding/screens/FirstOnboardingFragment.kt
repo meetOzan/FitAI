7aaa38dbb2bb4ba045bc12cf441cb579da3cf6b8
Mert Ozan Kahraman <mozankahraman69@gmail.com>
2024-06-03 09:40:06 +0300

    OnBoarding linked with graph

--- app/src/main/java/com/mobven/fitai/presentation/onboarding/screens/FirstOnboardingFragment.kt
+++ app/src/main/java/com/mobven/fitai/presentation/onboarding/screens/FirstOnboardingFragment.kt
@@ -1,6 +1,21 @@
 package com.mobven.fitai.presentation.onboarding.screens
 
+import androidx.navigation.NavDirections
+import androidx.navigation.fragment.findNavController
 import com.mobven.fitai.presentation.base.BaseFragment
 import com.mobven.fitai.databinding.FragmentFirstOnBoardingBinding
 
-class FirstOnboardingFragment : BaseFragment<FragmentFirstOnBoardingBinding>(FragmentFirstOnBoardingBinding::inflate)
+class FirstOnboardingFragment : BaseFragment<FragmentFirstOnBoardingBinding>(FragmentFirstOnBoardingBinding::inflate) {
+
+    override fun observeUi() {
+        binding.btnOnboarding1Skip.setOnClickListener {
+            val onBoardingAction = ObDirections.actionOnboardingFragmentToAuthFragment()
+            navigate(onBoardingAction)
+        }
+    }
+
+    override fun navigate(action: NavDirections) {
+        findNavController().navigate(action)
+    }
+
+}
--- app/src/main/java/com/mobven/fitai/presentation/onboarding/screens/ThirdOnboardingFragment.kt
+++ app/src/main/java/com/mobven/fitai/presentation/onboarding/screens/ThirdOnboardingFragment.kt
@@ -1,6 +1,21 @@
 package com.mobven.fitai.presentation.onboarding.screens
 
+import androidx.navigation.NavDirections
+import androidx.navigation.fragment.findNavController
 import com.mobven.fitai.presentation.base.BaseFragment
 import com.mobven.fitai.databinding.FragmentThirdOnBoardingBinding
 
-class ThirdOnboardingFragment : BaseFragment<FragmentThirdOnBoardingBinding>(FragmentThirdOnBoardingBinding::inflate)
+class ThirdOnboardingFragment : BaseFragment<FragmentThirdOnBoardingBinding>(FragmentThirdOnBoardingBinding::inflate) {
+
+    override fun observeUi() {
+        binding.btnOnboarding3Skip.setOnClickListener {
+            val onBoardingAction = ObDirections.actionOnboardingFragmentToAuthFragment()
+            navigate(onBoardingAction)
+        }
+    }
+
+    override fun navigate(action: NavDirections) {
+        findNavController().navigate(action)
+    }
+
+}
