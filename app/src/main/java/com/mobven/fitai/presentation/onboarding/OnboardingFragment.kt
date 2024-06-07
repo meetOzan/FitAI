c3199c11ba07cb961a962f6cf8afad0961eae08b
Mert Ozan Kahraman <mozankahraman69@gmail.com>
2024-06-07 14:07:51 +0300

    IsFirstLaunch added

--- app/src/main/java/com/mobven/fitai/presentation/onboarding/OnboardingFragment.kt
+++ app/src/main/java/com/mobven/fitai/presentation/onboarding/OnboardingFragment.kt
@@ -1,9 +1,27 @@
 package com.mobven.fitai.presentation.onboarding
 
+import android.os.Bundle
+import com.mobven.fitai.R
+import com.mobven.fitai.common.SharedPreferencesHelper
 import com.mobven.fitai.presentation.base.BaseFragment
 import com.mobven.fitai.databinding.FragmentOnBoardingBinding
+import com.mobven.fitai.presentation.onboarding.adapter.OnboardingPagerAdapter
+import dagger.hilt.android.AndroidEntryPoint
 
-class OnboardingFragment : BaseFragment<FragmentOnBoardingBinding>(FragmentOnBoardingBinding::inflate) {
+
+@AndroidEntryPoint
+class OnboardingFragment :
+    BaseFragment<FragmentOnBoardingBinding>(FragmentOnBoardingBinding::inflate) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        SharedPreferencesHelper.getIsFirstLaunch(requireContext()).let {
+            if (it == true) {
+                val action = OnboardingFragmentDirections.actionOnboardingFragmentToAuthFragment()
+                navigate(action)
+            }
+        }
+    }
 
     override fun observeUi() {
         binding.onboardingViewPager.adapter = OnboardingPagerAdapter(requireActivity())
